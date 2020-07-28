d481cd93fe5255c1aa0c8b1ce73e83a54d44c1bd
lotharelvin <63529290+lotharelvin@users.noreply.github.com>
2020-07-28 17:34:51 +0800

    global

--- Global_var.py
+++ Global_var.py
@@ -1,4 +1,11 @@
 
 Commodity_type=['Fruit','Snack','Alcohol','Beverage','Cigarette','Staple_food']
 
-Commodity_dict=
+Commodity_dict={
+	'Apple':'Fruit',
+	'Banana':'Fruit',
+	'Chips':'Snack',
+	'Cola':'Beverage',
+	'Moutai':'Alcohol'
+}
+
