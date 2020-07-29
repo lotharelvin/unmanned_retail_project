df9d50c587175c76ab62af3aa49a74e07ada8642
lotharelvin <63529290+lotharelvin@users.noreply.github.com>
2020-07-29 10:51:08 +0800

    Cus_sql

--- Backstage_management/Customer_Info_management/Cus_sql.py
+++ Backstage_management/Customer_Info_management/Cus_sql.py
@@ -1,17 +1,18 @@
 import mysql.connector
 
 
-class Connection_sql(object):
+class Connection_customer(object):
 	"""docstring for Connection_sql"""
 	def __init__(self):
-		super(Connection_sql, self).__init__()
+		super(Connection_customer, self).__init__()
 		self.__My_con=None
+		self.__My_cursor=None
 		
 
 
-	def Connect_to_sql(self):
+	def Connect_to_customer(self):
 		try:
-			__My_con=mysql.connector.connect(
+			self.__My_con=mysql.connector.connect(
 				host="localhost",
 				user="root",
 				passwd="123456aa"
@@ -19,8 +20,36 @@ class Connection_sql(object):
 		except ConnectionError as e:
 			print("Connection Error",e)
 
-	def Choose_database(self):
-		pass
+		self.__My_cursor=self.__My_con.cursor()
+
+		try:
+			self.__My_cursor.execute("USE Customer")
+
+		except ConnectionError as e:
+			print("Choose database Error",e)
+
+	def Show_tables(self):
+		if self.__My_con is None:
+			print("Connection unestablished")
+			return 
+		else:
+			self.__My_cursor.execute("SHOW TABLES")
+			for x in self.__My_cursor:
+				print(x)
+
+	def Use_table(self,Table_name):
+		self.__My_cursor.execute("ALTER TABLE "+Table_name)
+		print(self.__My_cursor)
+
+		
+
+
+con1=Connection_customer()
+con1.Connect_to_customer()
+con1.Show_tables()
+con1.Use_table("dx")
+
+
 
 	
 
